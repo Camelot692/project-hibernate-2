66b975278e52ef471c384e534f6c44fe501481d8
CamelotSIS <camelotsis@gmail.com>
2024-08-30 20:15:11 +0300

    initial commit

---
+++ src/main/java/org/camelot692/App.java
@@ -0,0 +1,13 @@
+package org.camelot692;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
